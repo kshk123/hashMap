4835c03ab172919691a559ec851cb566796c9795
kshk123 <kshk123@gmail.com>
2017-01-19 22:28:29 +0530

    Changed all tabs to spaces

--- inc/HashNode.h
+++ inc/HashNode.h
@@ -1,5 +1,5 @@
-#ifndef _HASH_NODE_H__
-#define _HASH_NODE_H__
+#ifndef HASH_NODE_H_
+#define HASH_NODE_H_
 
 #include <shared_mutex>
 
@@ -7,21 +7,21 @@
 template <typename K, typename V>
 class HashNode
 {
-	public:
-		HashNode() : next(NULL)
-		{};
-		HashNode(K key_, V value_) : next(NULL), key(key_), value(value_)
-		{};
-		~HashNode() {next = NULL;}
+    public:
+        HashNode() : next(NULL)
+        {};
+        HashNode(K key_, V value_) : next(NULL), key(key_), value(value_)
+        {};
+        ~HashNode() {next = NULL;}
 
-		K getKey() const {return key;}
-		void setValue(V value_) {value = value_;}
-		V getValue() const {return value;}
+        K getKey() const {return key;}
+        void setValue(V value_) {value = value_;}
+        V getValue() const {return value;}
 
-		HashNode *next; //Pointer to the next node in the same bucket
-	private:
-		K key;   //the hash key
-		V value; //the value corresponding to the key
+        HashNode *next; //Pointer to the next node in the same bucket
+    private:
+        K key;   //the hash key
+        V value; //the value corresponding to the key
 };
 
 
@@ -30,32 +30,32 @@ class HashNode
 template <typename K, typename V>
 class HashBucket
 {
-	public:
-		HashBucket()
-		{
-			head = new HashNode<K, V>();//A bucket is created with its head as a dummy hash node.
-		}
+    public:
+        HashBucket()
+        {
+            head = new HashNode<K, V>();//A bucket is created with its head as a dummy hash node.
+        }
 
-		~HashBucket() //delete the bucket
-		{
-			std::unique_lock<std::shared_timed_mutex> lock(mutex_); //take a lock before removing the nodes
-			HashNode<K, V> * prev = NULL;
-			HashNode<K, V> * node = head;
-			while(node != NULL)
-			{
-				prev = node;
-				node = node->next;
-				delete prev;
-			}
-		}	
+        ~HashBucket() //delete the bucket
+        {
+            std::unique_lock<std::shared_timed_mutex> lock(mutex_); //take a lock before removing the nodes
+            HashNode<K, V> * prev = NULL;
+            HashNode<K, V> * node = head;
+            while(node != NULL)
+            {
+                prev = node;
+                node = node->next;
+                delete prev;
+            }
+        }   
 
-		//Function to find an entry in the bucket matching the key
-		//If key is found, the corresponding value is copied into the parameter "value" and function returns true.
-		//If key is not found, function returns false
-		bool find(const K &key, V &value) const
-		{
-			// A shared mutex is used to enable mutiple concurrent reads
-			std::shared_lock<std::shared_timed_mutex> lock(mutex_); 
+        //Function to find an entry in the bucket matching the key
+        //If key is found, the corresponding value is copied into the parameter "value" and function returns true.
+        //If key is not found, function returns false
+        bool find(const K &key, V &value) const
+        {
+            // A shared mutex is used to enable mutiple concurrent reads
+            std::shared_lock<std::shared_timed_mutex> lock(mutex_); 
             HashNode<K, V> * node = head->next; //The head node is dummy, no need to look there
 
             while (node != NULL)
@@ -68,15 +68,15 @@ class HashBucket
                 node = node->next;
             }
             return false;
-		}
+        }
 
-		//Function to insert into the bucket
-		//If key already exists, update the value, else insert a new node in the bucket with the <key, value> pair
-		void insert(const K &key, const V &value)
-		{
-			//Exclusive lock to enable single write in the bucket
-			std::unique_lock<std::shared_timed_mutex> lock(mutex_);
-			HashNode<K, V> * prev = head;
+        //Function to insert into the bucket
+        //If key already exists, update the value, else insert a new node in the bucket with the <key, value> pair
+        void insert(const K &key, const V &value)
+        {
+            //Exclusive lock to enable single write in the bucket
+            std::unique_lock<std::shared_timed_mutex> lock(mutex_);
+            HashNode<K, V> * prev = head;
             HashNode<K, V> * node = head->next; //The head node is dummy, no need to look there
 
             while (node != NULL && node->getKey() != key)
@@ -93,15 +93,15 @@ class HashBucket
             {
                 node->setValue(value); //Key found in bucket, update the value
             }
-	
-		}
+    
+        }
 
-		//Function to remove an entry from the bucket, if found
-		void erase(const K &key)
-		{
-			//Exclusive lock to enable single write in the bucket
-			std::unique_lock<std::shared_timed_mutex> lock(mutex_);
-			HashNode<K, V> *prev  = head;
+        //Function to remove an entry from the bucket, if found
+        void erase(const K &key)
+        {
+            //Exclusive lock to enable single write in the bucket
+            std::unique_lock<std::shared_timed_mutex> lock(mutex_);
+            HashNode<K, V> *prev  = head;
             HashNode<K, V> * node = head->next;
 
             while (node != NULL && node->getKey() != key)
@@ -116,31 +116,31 @@ class HashBucket
             }
             else
             {
-            	prev->next = node->next; //Remove the node from the bucket
+                prev->next = node->next; //Remove the node from the bucket
                 delete node; //Free up the memory
             }
 
-		}
+        }
 
-		//Function to clear the bucket, all nodes except the dummy head node will be removed
-		//The head node will be destroyed only when the bucket itself is destroyed
-		void clear()
-		{
-			//Exclusive lock to enable single write in the bucket
-			std::unique_lock<std::shared_timed_mutex> lock(mutex_);
-			HashNode<K, V> * prev = head;
-			HashNode<K, V> * node = head->next; //The dummy head node will not be removed
-			while(node != NULL)
-			{
-				prev->next = node->next;
-				delete node;
-				node = prev->next;
-			}
-		}
+        //Function to clear the bucket, all nodes except the dummy head node will be removed
+        //The head node will be destroyed only when the bucket itself is destroyed
+        void clear()
+        {
+            //Exclusive lock to enable single write in the bucket
+            std::unique_lock<std::shared_timed_mutex> lock(mutex_);
+            HashNode<K, V> * prev = head;
+            HashNode<K, V> * node = head->next; //The dummy head node will not be removed
+            while(node != NULL)
+            {
+                prev->next = node->next;
+                delete node;
+                node = prev->next;
+            }
+        }
 
-	private:
-		HashNode<K, V> * head; //The head node of the bucket
-		mutable std::shared_timed_mutex mutex_; //The mutex for this bucket
+    private:
+        HashNode<K, V> * head; //The head node of the bucket
+        mutable std::shared_timed_mutex mutex_; //The mutex for this bucket
 };
 
 #endif
