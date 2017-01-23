079903a5d182aa9e9f0ca6dbc2a881986216dd2e
kshk123 <kshk123@gmail.com>
2017-01-23 23:58:52 +0530

    Disabled support of copy and move for now

--- inc/HashMap.h
+++ inc/HashMap.h
@@ -40,6 +40,11 @@ namespace CTSL //Concurrent Thread Safe Library
 
                 delete []hashTable;
             }
+            //Copy and Move of the HashMap are not supported at this moment
+            HashMap(const HashMap&) = delete;
+            HashMap(HashMap&&) = delete;
+            HashMap& operator=(const HashMap&) = delete;  
+            HashMap& operator=(HashMap&&) = delete;
         
             //Function to find an entry in the hash map matching the key.
             //If key is found, the corresponding value is copied into the parameter "value" and function returns true.
@@ -78,11 +83,6 @@ namespace CTSL //Concurrent Thread Safe Library
             }   
 
         private:
-            //Copy and Move of the HashMap are not supported at this moment
-            HashMap(const HashMap&) = default;
-            HashMap(HashMap&&) = default;
-            HashMap& operator=(const HashMap&) & = default;  
-            HashMap& operator=(HashMap&&) & = default;
             
 
             HashBucket<K, V> ** hashTable;
